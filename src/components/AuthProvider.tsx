02302137c697c125b09cce4f15ab3e21f8f83d06
Kabiru Alli <kalli@seamfix.com>
2025-11-08 17:36:37 +0100

    more updates

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-// import { SessionProvider } from "next-auth/react";
-
-interface AuthProviderProps {
-  children: React.ReactNode;
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
-
-function AuthProvider({ children }: AuthProviderProps) {
-  return (
-    <>{children}</>
-  );
-}
-
-export default AuthProvider;
